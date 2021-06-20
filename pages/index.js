c3ea765781002b44484efe1d171e1e7ee40bd0d3
Desiree Lerma <desi.lerma96@yahoo.com>
2021-06-20 01:47:40 -0500

    Updates head and home page.

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>WatchNext</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+        Welcome to WatchNext!
         </h1>
 
         <p className="description">
