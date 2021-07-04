3ec75eb4c885f89fb739770879e16bd2911ed50d
Desiree Lerma <desi.lerma96@yahoo.com>
2021-07-04 10:14:51 -0500

    Adds getStaticProps funct. to pop movies pg to render API data.

--- pages/popular_movies.js
+++ pages/popular_movies.js
@@ -1,3 +1,17 @@
-import axios from "axios";
+// import axios from "axios";
 
-const fetchPopMovies = (page) => axios.get('/api/popular-movies')
+// const fetchPopMovies = (page) => axios.get('/api/popular-movies')
+
+// runs at build time
+// can add fetch req to fetch data
+// runs b4 component is rendered
+export const getStaticProps = async () => {
+    const res = await fetch('http://localhost:3000/api/popular-movies');
+    const data = await res.json();
+
+    return {
+        // makes data avail in props
+        props: {text: data}
+    }
+
+}
