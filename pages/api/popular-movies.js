ff88ea427b9054308520332a0b1d0b96529eab3b
Desiree Lerma <desi.lerma96@yahoo.com>
2021-07-04 09:58:24 -0500

    Refactors API endpoint for pop movies. Check if it properly returns info.

--- pages/api/popular-movies.js
+++ pages/api/popular-movies.js
@@ -1,8 +1,13 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default async (req, res) => {
-    const URL = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.API_KEY}`;
-    const response = await axios.get(URL);
-    // Get data from db
-    res.status(200).json({ data: response.data })
+// export default async (req, res) => {
+//     const URL = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.API_KEY}`;
+//     const response = await axios.get(URL);
+//     // Get data from db
+//     res.status(200).json({ data: response.data })
+// }
+
+// req = HTTP incoming msg, res = HTTP server response
+export default function handler(req, res) {
+    res.status(200).json({text: 'Does this work?'})
 }
